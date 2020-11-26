2fb0d9011a775a8f4a794a6030ad1af00b7c6a89
samphirereeve <samphire.reeve4@gmail.com>
2020-11-26 21:10:26 +0000

    Added filtering on title and date

--- frontend/coalesce/src/pages/OpportunitiesSearchPage.vue
+++ frontend/coalesce/src/pages/OpportunitiesSearchPage.vue
@@ -10,7 +10,7 @@
           </q-card-section>
           <!-- search bar -->
           <q-toolbar>
-            <q-input class="search-bar" rounded outlined v-model="text">
+            <q-input rounded outlined class="search-bar" v-model="filter" type="text" >
               <template v-slot:append>
                 <q-icon name="search" />
               </template>
@@ -20,14 +20,14 @@
 
         <!-- list all opportunities -->
         <div class="row" >
-          <div class="col-md-6 q-pa-md" v-for="o in opportunities" v-bind:key="o.id">
+          <div class="col-md-6 q-pa-md" v-for="o in filterOpportunity" v-bind:key="o.id">
             <q-card class="opportunity-card card">
               <q-card-section class="bg-primary text-white">
                 <div class="text-h6">{{ o.title }}</div>
               </q-card-section>
 
               <q-card-section>
-                <div class="text-h6">{{ o.description }}</div>
+                <div class="text-h7 ellipsis-2-lines">{{ o.description }}</div>
               </q-card-section>
 
               <q-separator />
@@ -43,7 +43,25 @@
       <!-- filtering -->
       <div class="col-4">
         <q-card class="q-pa-md q-ma-md">
-          <div class="text-h5 text-weight-bold">Filter</div>
+          <div class="text-h5 text-weight-bold q-pl-md">Filter</div>
+
+          <!-- Calender filter -->
+          <div class="q-pa-md" >
+            <q-input filled v-model="filterDate">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                    <q-date v-model="filterDate">
+                      <div class="row items-center justify-end">
+                        <q-btn label="Clear" color="primary" @click="clearDateFilter" flat />
+                        <q-btn v-close-popup label="Close" color="primary" flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
         </q-card>
       </div>
     </div>
@@ -55,21 +73,25 @@
 const opportunities = [
   {
     id: 123,
-    title: 'Opportunity 1',
+    date: '2020/12/25',
+    title: 'My opportunity 1',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   },
   {
     id: 234,
+    date: '2020/11/30',
     title: 'Opportunity 2',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   },
   {
     id: 345,
-    title: 'Opportunity 3',
+    date: '2020/12/04',
+    title: 'Opportunity 23',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   },
   {
     id: 456,
+    date: '2021/01/14',
     title: 'Opportunity 4',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   }
@@ -79,7 +101,31 @@ export default {
   name: 'OpportunitiesSearchPage',
   data () {
     return {
-      opportunities: opportunities
+      opportunities: opportunities,
+      filter: '',
+      filterDate: ''
+    }
+  },
+  computed: {
+    filterOpportunity: function () {
+      console.log('here')
+      if (!this.filter && !this.filterDate) {
+        return this.opportunities
+      }
+
+      let results = this.opportunities.filter(o => {
+        return o.title.toLowerCase().indexOf(this.filter.toLowerCase()) > -1
+      })
+
+      results = results.filter(o => {
+        return o.date.toLowerCase().indexOf(this.filterDate.toLowerCase()) > -1
+      })
+      return results
+    }
+  },
+  methods: {
+    clearDateFilter () {
+      this.filterDate = ''
     }
   }
 }
