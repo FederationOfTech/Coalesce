0a1b2ac4b332c92b8619d9d15548c3e2da2aad70
Mike Nolan <me@michael-nolan.com>
2020-11-27 12:25:54 +0000

    fix: flake8

--- coalesce/urls.py
+++ coalesce/urls.py
@@ -2,10 +2,8 @@ from django.conf import settings
 from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
