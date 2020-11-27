1b7ccc49f6b8a78862e2e329101a239ee9fd6a63
christa.kollen <christa.kollen@just-eat.com>
2020-11-27 16:41:02 +0000

    rebase

--- coalesce/urls.py
+++ coalesce/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
-from .opportunities.views import OpportunityViewSet
+from rest_framework.authtoken import views
 from .users.views import UserViewSet, UserCreateViewSet
 from .organizers.views import OrganizerCreateViewSet
 from .volunteers.views import VolunteerCreateViewSet
 
 router = DefaultRouter()
+
 router.register(r"users", UserViewSet)
 router.register(r"users", UserCreateViewSet)
 router.register(r'opportunities', OpportunityViewSet)
@@ -17,10 +18,13 @@ router.register(r"organizers", OrganizerCreateViewSet)
 router.register(r"volunteers", VolunteerCreateViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    # the 'api-root' from django rest-frameworks default router
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+                  # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
-    re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
+    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
