973cd4a2e6be13600443f41d12b0e39f8801ee20
samphirereeve <samphire.reeve4@gmail.com>
2021-04-20 13:18:29 +0100

    Added in volunteer details outline

---
+++ frontend/coalesce/src/pages/VolunteerDetailPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page>
+    <div class="text-h5 q-pt-lg">{{ volunteer.description }}</div>
+    <div class="text-h5 q-pt-lg">{{ volunteer.skills }}</div>
+    <div class="text-h5 q-pt-lg">{{ volunteer.organization }}</div>
+    <div class="text-h5 q-pt-lg">{{ volunteer.organizer_comments }}</div>
+  </q-page>
+</template>
+
+<script>
+const volunteer = {
+  id: 123,
+  description: 'Lorem ipsum',
+  skills: [
+    'skill1', 'skill2'
+  ],
+  organization: 'The volunteers employer or organization',
+  organizer_comments: 'Notes from organizers about this volunteer'
+}
+
+export default {
+  name: 'VolunteerDetailPage',
+  data () {
+    return {
+      volunteer: volunteer
+    }
+  }
+}
+</script>
