51fdd2f7a2acf69fd68958811ef3ccef0399154a
Jack Lawton <lawton27@aiimi.com>
2021-04-20 15:28:53 +0100

    Removed unused imports

--- coalesce/organizations/models.py
+++ coalesce/organizations/models.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+
 from django.db import models
 
 
--- coalesce/organizations/serializers.py
+++ coalesce/organizations/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Organization
-from ..organizers.models import Organizer
+
 
 class OrganizationSerializer(serializers.ModelSerializer):
 
