5cd6a2467077d0556d6c900479fa75a37152f808
samphirereeve <samphire.reeve4@gmail.com>
2021-04-22 13:28:45 +0100

    Added create opportunity page

---
+++ frontend/coalesce/src/pages/CreateAnOpportunityPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="q-px-xl">
+    <div class="text-overline">OPPORTUNITY DETAILS</div>
+    <q-card class="my-card">
+      <q-card-section>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'CreateAnOpportunityPage',
+  data () {
+    return {
+    }
+  }
+}
+</script>
