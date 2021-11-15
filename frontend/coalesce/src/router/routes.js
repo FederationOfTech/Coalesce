c6c8cd9fb1523246c8ab8ee5fb45ec2ab281d974
Tim Rogers <tim.rogers@just-eat.com>
2021-11-15 11:34:35 +0000

    Make URLs, spellings and filenames consistent

--- frontend/coalesce/src/router/routes.js
+++ frontend/coalesce/src/router/routes.js
@@ -4,13 +4,13 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: 'create-organiser', component: () => import('pages/CreateOrganiser.vue') },
       { path: 'login', component: () => import('pages/LogIn.vue') },
-      { path: 'opportunities', component: () => import('pages/OpportunitiesSearchPage.vue') },
-      { path: 'opportunity/:id', component: () => import('pages/OpportunityDetailsPage.vue') },
-      { path: 'opportunity', component: () => import('pages/CreateAnOpportunityPage.vue') },
-      { path: 'organizer', component: () => import('pages/OrganizerDashboardPage.vue') },
-      { path: 'volunteer/:id', component: () => import('pages/VolunteerDetailPage.vue') },
+      { path: 'opportunities', component: () => import('pages/OpportunitiesSearch.vue') },
+      { path: 'opportunities/create', component: () => import('pages/CreateOpportunity.vue') },
+      { path: 'opportunities/:id', component: () => import('pages/OpportunityDetails.vue') },
+      { path: 'organisers', component: () => import('pages/OrganiserDashboard.vue') },
+      { path: 'organisers/create', component: () => import('pages/CreateOrganiser.vue') },
+      { path: 'volunteers/:id', component: () => import('pages/VolunteerDetail.vue') },
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
